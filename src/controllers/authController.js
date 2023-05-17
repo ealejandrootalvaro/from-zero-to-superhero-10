be537e4d4c6aef3ccd01e97bac980bd39b4b8915
Edwin Otalvaro <edwin.otalvaro@cbinsights.com>
2023-05-16 20:25:38 -0500

    tarea

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,28 +1,9 @@
 const jwt = require("jsonwebtoken");
 
 exports.login = (req, res) => {
-
-    let { email } = req.body;
-
-    token = jwt.sign(
-        { email: email },
-        process.env.JWT_SECRET_KEY,
-        { expiresIn: "1h" }
-    );
-
-    res
-    .status(200)
-    .json({
-      success: true,
-      data: {
-        token: token,
-      },
-    });
+  // Tarea Clase 8. Leer README.md
 };
 
 exports.restrictedView = (_, res) => {
-
-    res.status(200)
-    .send("Confidential View")
-
+  // Tarea Clase 8. Leer README.md
 }
--- src/middlewares/handlers.js
+++ src/middlewares/handlers.js
@@ -13,24 +13,5 @@ exports.notFoundHandler = (req, res, _) => {
 }
 
 exports.isAuth = (req, res, next) => {
-    const auth = req.headers.authorization;
-
-    let decodedToken;
-
-    try {
-        decodedToken = jwt.verify(auth, process.env.JWT_SECRET_KEY);
-    } catch(err) {
-        console.log(err);
-        res.status(401);
-        res.send(err.message || 'Access forbidden');
-        return;
-    }
-
-    if (decodedToken) {
-        next();
-    } else {
-        res.status(401);
-        res.send('Access forbidden');
-        return;
-    }
+    // Tarea Clase 8. Leer README.md
 }
