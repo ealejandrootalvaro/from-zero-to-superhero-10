0d817f82211c01adfd486f6003ff04c74d109345
Edwin Otalvaro <edwin.otalvaro@cbinsights.com>
2023-05-23 14:14:52 -0500

    implement jwt

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,8 +2,28 @@ const jwt = require("jsonwebtoken");
 
 exports.login = (req, res) => {
   // Tarea Clase 8. Leer README.md
+  const { email } = req.body; 
+
+  // Verificar usuario y contrasena en base datos.  
+  token = jwt.sign(
+    { email },
+    process.env.JWT_SECRET_KEY,
+    { expiresIn: "1s" }
+  )
+
+  res
+        .status(200)
+        .json({
+        success: true,
+        data: {
+            token,
+        },
+        });
+
 };
 
 exports.restrictedView = (_, res) => {
   // Tarea Clase 8. Leer README.md
+  res.status(200)
+    .send("Confidential View")
 }
--- src/middlewares/handlers.js
+++ src/middlewares/handlers.js
@@ -14,4 +14,26 @@ exports.notFoundHandler = (req, res, _) => {
 
 exports.isAuth = (req, res, next) => {
     // Tarea Clase 8. Leer README.md
+
+    const token = req.headers.authorization;
+
+    let decodedToken;
+
+    try {
+        decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    } catch(err) {
+        console.log(err);
+        res.status(401);
+        res.send(err.message || 'Access forbidden');
+        return;
+    }
+
+    if (decodedToken) {
+        next();
+    } else {
+        res.status(401);
+        res.send('Access forbidden');
+        return;
+    }
+
 }
